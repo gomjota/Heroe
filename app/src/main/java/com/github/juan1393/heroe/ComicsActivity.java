66d4596141f2ae7c95ec4f10864c9c246047d1fe
Juan <j.gtrapero@gmail.com>
2017-02-11 11:17:05 +0100

    first commit

---
+++ app/src/main/java/com/github/juan1393/heroe/ComicsActivity.java
@@ -0,0 +1,13 @@
+package com.github.juan1393.heroe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ComicsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comics);
+    }
+}
